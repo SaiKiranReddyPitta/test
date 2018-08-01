ed4339b78f22e0b3d58b77173ee3672b704d3306
unknown <pittasai01@msitprogram.net>
2018-08-01 20:32:47 +0530

    M3 A11 Commit

---
+++ cspp1-practice/M3/compare_AB.py
@@ -0,0 +1,10 @@
+varA=96
+varB=69
+if(type(varA) == str or type(varB) == str):
+	print("string involved")
+if((varA)>(varB)):
+	print("bigger")
+elif((varA)<(varB)):
+	print("smaller")
+else:
+	print("equal")
