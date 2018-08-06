23459cfece0714da48b28769c55a15cf76936ead
SaiKiranReddyPitta <pittasai01@msitprogram.net>
2018-08-06 17:21:06 +0530

    odd

---
+++ cspp1-practice/cspp1-assignments/m7/Functions - Odd Exercise/Functions - Odd Exercise/odd.py
@@ -0,0 +1,16 @@
+'''
+Author: Sai Kiran Reddy Pitta
+Date: 06 Aug 2018
+Encoding: UTF-8
+'''
+
+
+def odd(x):
+   return  x%2 == 1 
+
+def main():
+    data = input()
+    print(odd(int(data)))
+
+if __name__== "__main__":
+    main()
