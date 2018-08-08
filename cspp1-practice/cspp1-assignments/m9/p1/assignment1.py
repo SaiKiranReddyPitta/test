b01e7c3ee44db75679ae544673cd3ae974c51ea9
SaiKiranReddyPitta <pittasai01@msitprogram.net>
2018-08-08 19:49:37 +0530

    " -> 9 of 9 passed. pylint: 9.55/10 "

--- cspp1-practice/cspp1-assignments/m9/p1/assignment1.py
+++ cspp1-practice/cspp1-assignments/m9/p1/assignment1.py
@@ -15,6 +15,14 @@ def is_word_guessed(secret_word, letters_guessed):
       False otherwise
     '''
     # FILL IN YOUR CODE HERE...
+    #count = 0
+    #for i in range (len(secret_word)):
+    #    if letters_guessed[i] in secret_word:
+    #        count += 1
+    #if count == len(secret_word):
+    #    return True
+    #else:   
+    #    return False
     ans = list(secret_word)
     count = 0
     for i in ans:
