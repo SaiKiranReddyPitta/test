06c12b466be601f2c2d004b7b9ebf30acb766426
SaiKiranReddyPitta <pittasai01@msitprogram.net>
2018-08-10 19:54:50 +0530

    " -> 0 of 6 passed. pylint: 3.53/10 "

--- cspp1-practice/cspp1-assignments/m11/p2/p2/assignment2.py
+++ cspp1-practice/cspp1-assignments/m11/p2/p2/assignment2.py
@@ -6,7 +6,7 @@ Encoding: utf - 8
 ************************
 '''
 
-def updateHand(hand, word):
+def updatehand(hand, word):
     """
     Assumes that 'hand' has all the letters in word.
     In other words, this assumes that however many times
@@ -23,29 +23,25 @@ def updateHand(hand, word):
     returns: dictionary (string -> int)
     """
     # TO DO ... <-- Remove this comment when you code this function
-    scrabble_letter_values = {
-        'a': 1, 'b': 3, 'c': 3, 'd': 2, 'e': 1, 'f': 4, 'g': 2, 'h': 4, 'i': 1,
-        'j': 8, 'k': 5, 'l': 1, 'm': 3, 'n': 1, 'o': 1, 'p': 3, 'q': 10, 'r': 1,
-        's': 1, 't': 1, 'u': 1, 'v': 4, 'w': 4, 'x': 8, 'y': 4, 'z': 10
-    }
-    a_a = 0
+
     hand = list(hand)
     for i in hand:
         if i in list(hand.keys()):
-        hand[i]-1
+            hand[i] -= 1
     return hand
 
 def main():
-    n=input()
+    '''
+    main function
+    '''
+    n_input=input()
     adict={}
-    for i in range(int(n)):
+    for i in range(int(n_input)):
         data=input()
-        l=data.split()
-        adict[l[0]]=int(l[1])
+        l_l=data.split()
+        adict[l_l[0]]=int(l_l[1])
     data1=input()
-    print(updateHand(adict,data1))
-        
-
+    print(updatehand(adict,data1))
 
 if __name__ == "__main__":
     main()
