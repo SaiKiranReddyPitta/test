7d6f7d87dfbf0866b23ce7f45a2b53453c5febb3
SaiKiranReddyPitta <pittasai01@msitprogram.net>
2018-08-23 14:51:01 +0530

    " -> 4 of 8 passed. pylint: -3.00/10 "

--- cspp1-practice/cspp1-assignments/m20/CodeCampMatrixOperations/matrix_operations.py
+++ cspp1-practice/cspp1-assignments/m20/CodeCampMatrixOperations/matrix_operations.py
@@ -1,33 +1,31 @@
 row=list(map(int,input().split(','))) #input no. of row and column
 b=[]
 for i in range(0,row[0]):
-	a=list(map(int,input().split()))
-	b.append(a)
+    a=list(map(int,input().split()))
+    b.append(a)
 #print(b)
 
 row=list(map(int,input().split(','))) #input no. of row and column
 c=[]
 for i in range(0,row[0]):
-	a=list(map(int,input().split()))
-	c.append(a)
+    a=list(map(int,input().split()))
+    c.append(a)
 #print(c)
 l = []
-l2 = []
 result = [[b[i][j] + c[i][j]  for j in range(len(b[0]))] for i in range(len(b))]
   
-# for r in result:
+for r in result:
 #     print(r)
-l.append(result)
+    l.append(r)
 print(l)
-l2 = [e for e in l if e]
-print(l2)
+
 
 # Multiplication
 m = [] 
 result = [[sum(b * c for b, c in zip(b_row, c_col)) 
                         for c_col in zip(*c)]
                                 for b_row in b]
-# for r in result:
+for r in result:
 #     print(r)
-m.append(result)
+    m.append(r)
 print(m)
