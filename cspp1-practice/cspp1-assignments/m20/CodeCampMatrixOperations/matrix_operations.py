f0d5bc3706f5ba8b96a3f6294b0933a1a38a8250
SaiKiranReddyPitta <pittasai01@msitprogram.net>
2018-08-23 14:37:23 +0530

    " -> 0 of 8 passed. pylint: -6.67/10 "

--- cspp1-practice/cspp1-assignments/m20/CodeCampMatrixOperations/matrix_operations.py
+++ cspp1-practice/cspp1-assignments/m20/CodeCampMatrixOperations/matrix_operations.py
@@ -1,28 +1,30 @@
 row=list(map(int,input().split(','))) #input no. of row and column
 b=[]
 for i in range(0,row[0]):
-    a=list(map(int,input().split()))
-    b.append(a)
+	a=list(map(int,input().split()))
+	b.append(a)
 #print(b)
 
 row=list(map(int,input().split(','))) #input no. of row and column
 c=[]
 for i in range(0,row[0]):
-    a=list(map(int,input().split()))
-    c.append(a)
+	a=list(map(int,input().split()))
+	c.append(a)
 #print(c)
+l = []
 result = [[b[i][j] + c[i][j]  for j in range(len(b[0]))] for i in range(len(b))]
   
-for r in result:
-    print(r)
+# for r in result:
+#     print(r)
+l.append(result)
+print(l)
 
 # Multiplication
-for i in range(len(b)):
- 
-    for j in range(len(c[0])):
- 
-        for k in range(len(c)):
-            result[i][j] += b[i][k] * c[k][j]
- 
-for r in result:
-    print(r)
+m = [] 
+result = [[sum(b * c for b, c in zip(b_row, c_col)) 
+                        for c_col in zip(*c)]
+                                for b_row in b]
+# for r in result:
+#     print(r)
+m.append(result)
+print(m)
