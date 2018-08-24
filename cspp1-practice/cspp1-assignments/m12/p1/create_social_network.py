49ae36d1fc166f33ba729c4096c876a79f460cb8
SaiKiranReddyPitta <pittasai01@msitprogram.net>
2018-08-24 12:16:02 +0530

    " -> 0 of 3 passed. pylint: 0/0 "

--- cspp1-practice/cspp1-assignments/m12/p1/create_social_network.py
+++ cspp1-practice/cspp1-assignments/m12/p1/create_social_network.py
@@ -3,7 +3,7 @@
     Date: 11-08-2018
 '''
 
-def create_social_network(data):
+def create_social_network(data):          
     '''
         The data argument passed to the function is a string
         It represents simple social network data
@@ -32,30 +32,24 @@ def create_social_network(data):
         Return a empty dictionary if the string format of the data is invalid
         Empty dictionary is not None, it is a dictionary with no keys
     '''
-    l_input = []
-    for i in data(input):
-        l_input.append(input())
-    adict_data = {}
-    for i in l_input:
-        l_new = i.split('follows')
-        print(l_new[1])
-        if l_new[0] in adict_data:
-            l_new[1] = l_new[1].split(',')
-            for a_a in l_new[1]:
-                adict_data[l_new[0]].append(a_a)
-            else:
-                l_new[1] = l_new[1].split(',')
-                adict_data[l_new[0]] = l_new[1]
-            for l_a in data(len(l_new[1])):
-                adict_data[l_new[0][l_a]] = adict_data[l_new[0][l_a]]
-    return adict_data
+    given_dict = data.splitlines()
+    dictionary = {}
+    if "follows" in given_dict[0]:
+        for input_i in given_dict:
+            list_i = input_i.split(" follows ")
+            list_1 = list_i[1].split(",")
+            if list_i[0] in dictionary:
+                dictionary[list_i[1]].append(list_1)
+            else
+                dictionary[list_i[0]] = list_1
+    return dictionary
 
 
 def main():
     '''
         handling testcase input and printing output
     '''
-    string = ''
+    string = '':
     lines = int(input())
     for i in range(lines):
         i += 1
