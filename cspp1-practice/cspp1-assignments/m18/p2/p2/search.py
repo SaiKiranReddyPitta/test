7ce0e834a33c528fc50a753e89f0c0a66e50b819
SaiKiranReddyPitta <pittasai01@msitprogram.net>
2018-08-24 20:53:33 +0530

    " -> 1 of 1 passed. pylint: 9.17/10 "

--- cspp1-practice/cspp1-assignments/m18/p2/p2/search.py
+++ cspp1-practice/cspp1-assignments/m18/p2/p2/search.py
@@ -3,8 +3,8 @@ Author = Sai Kiran Reddy Pitta
 Roll number = 20186002
 '''
 '''
-    Tiny Search Engine - Part 2 - Search
-    In this programming assingment you are given the search index and search queries as input.
+Tiny Search Engine - Part 2 - Search
+    In this programming assingment you are given the search index and search queries_list as input.
     Complete the program below to search in the index. Don't worry, it is explained below.
     A search index is a python dictionary.
     The keys of this dictionary are words contained in ALL the input text documents.
@@ -21,54 +21,57 @@ Roll number = 20186002
         .
         .
     }
-    The second input to this program is search queries.
-    Each search query has one or more words
-    A search query has to be tokenized i.e., the words have to be extracted
+    The second input to this program is search queries_list.
+    Each search querylist has one or more words
+    A search querylist has to be tokenized i.e., the words have to be extracted
     This word may be a key in the search index.
     If the word is a key then collect the values.
-    Collect the values from the search index for all the words in the search query.
+    Collect the values from the search index for all the words in the search querylist.
     Make a set using the doc_id present as the first item in the values tuple.
     Return the search results.
 
     Note: PyLint score need not be 10/10. Anything above 9.5 is good.
 '''
-
-def search(search_index, query):
+def search(search_index, querylist):
     '''
         function to search through the search index and return the results
-        split the query into lowercase words
+        split the querylist into lowercase words
         check if the word is in the search_index
         collect all the values for the words that are in the search_index
         make a set of doc_id and return
     '''
-
-
-
-def process_queries(search_index, queries):
+    list1 = set()
+    querylist = querylist.lower()
+    querylist = querylist.split()
+    for i in querylist:
+        if i in search_index:
+            list2 = list(search_index.get(i))
+            for j in list2:
+                list1.add(j[0])
+    return list1
+def process_queries_list(search_index, queries_list):
     '''
-        function to process the search queries
-        iterate through all the queries and call the search function
+        function to process the search queries_list
+        iterate through all the queries_list and call the search function
         print the results returned by search function
     '''
-    pass
-
+    for i in queries_list:
+        print(search(search_index, i))
 def main():
     '''
         main function
     '''
     # This line loads the search index
     search_index = eval(input())
-
-    # read the number of search queries
+    # read the number of search queries_list
     lines = int(input())
-    # read the search queries into a list
-    queries = []
+    # read the search queries_list into a list
+    queries_list = []
     for i in range(lines):
-        queries.append(input())
+        queries_list.append(input())
         i += 1
-
-    # call process queries
-    process_queries(search_index, queries)
-
+    #print(queries_list)
+    # call process queries_list
+    process_queries_list(search_index, queries_list)
 if __name__ == '__main__':
     main()
